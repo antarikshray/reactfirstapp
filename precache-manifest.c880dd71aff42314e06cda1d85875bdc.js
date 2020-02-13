ab08b8837232b4cc1fa6e7942cc09f4a79785c0f
Antariksh Ray <antarikshray>
2020-02-13 09:55:49 +0530

    Updates

---
+++ precache-manifest.c880dd71aff42314e06cda1d85875bdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a69b3e5311c14ce0dc9af15f89b16c0f",
+    "url": "/reactfirstapp/index.html"
+  },
+  {
+    "revision": "cf4c7b9f46cb4634332d",
+    "url": "/reactfirstapp/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "7afbfa5df059c5101bca",
+    "url": "/reactfirstapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf4c7b9f46cb4634332d",
+    "url": "/reactfirstapp/static/js/2.254624f8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactfirstapp/static/js/2.254624f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7afbfa5df059c5101bca",
+    "url": "/reactfirstapp/static/js/main.c466bdc8.chunk.js"
+  },
+  {
+    "revision": "ab6d8d4aa7f21fcabf28",
+    "url": "/reactfirstapp/static/js/runtime-main.8ebef52c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactfirstapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
